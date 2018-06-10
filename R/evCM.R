f8291266eefa54b8a88e5b66b44fa0367911c196
evgilenko <evgilenko@users.noreply.github.com>
2018-06-10 19:02:39 +0300

    Add files via upload

--- R/evCM.R
+++ R/evCM.R
@@ -1,7 +1,6 @@
 #' Confusion Matrix Calculation Function
 #'
 #' This function calculates a confusion matrix for the provided predicted (in rows) and true (in columns) values. It is also obligatory to provide the actual number of classes. If some of the classes are missed, the function adds the necessary number of zero rows (in case of predictions) or zero columns (in case of true classes) to the classification matrix.
-#' The function returns a data.frame containing (in the percentage format) overall R^2 count, average sensitivity over categories, and individual sensitivities for each of the classes (by columns of CM).
 #' @param Ypred a vector of predicted values.
 #' @param Ytrue a vector of actual values.
 #' @param nclasses the number of actual classes.
@@ -48,24 +47,7 @@ evCM <- function(Ypred, Ytrue, nclasses, CM.print=T, sample.name=""){
     print(tab.CM)
   }
   
-  ## get the diagonal and the sums of columns of CM
-  v.diagCM <- vector(mode="integer", length=0)
-  
-  for(j.CMcol in c(1:ncol(tab.CM))){
-    v.diagCM <- c(v.diagCM, tab.CM[j.CMcol,j.CMcol])
-  } # end for j.CMcol
-  
-  ## get the sums of columns of CM
-  v.sumsCM <- apply(tab.CM, 2, FUN=sum)
-  v.senses <- round(100*v.diagCM/v.sumsCM, 2)
-  v.senses[is.nan(v.senses)] <- 0
-  c.r2count <- round(sum(v.diagCM)*100/sum(tab.CM),2)
-  
-  ## composing the output table of sensitivities
-  tab.senses <- as.data.frame(t(c(c.r2count, round(mean(v.senses),2), v.senses)))
-  colnames(tab.senses) <- c("R2c,%", "AvgSens,%", c(0:(nclasses-1)))
-  
   ## returning the result
-  return(tab.senses)
+  return(tab.CM)
 
 } # end of funcCM
